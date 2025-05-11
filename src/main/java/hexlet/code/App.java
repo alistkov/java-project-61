9d03907a4fe3a08d6941d47ee74188bbd09cfc7a
Anton Listkov <antonlistkov@gmail.com>
2025-05-11 20:58:35 +0200

    display game menu

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
+    private static final int GREETING_OPTION = 1;
+    private static final int EXIT_OPTION = 0;
+
     public static void main(String[] args) {
-        Cli.greeting();
+        displayMenu();
+
+        Scanner sc = new Scanner(System.in);
+
+        var playerChoice = sc.nextInt();
+        System.out.println("\n");
+
+        switch (playerChoice) {
+            case GREETING_OPTION:
+                Cli.greeting(sc);
+                break;
+            case EXIT_OPTION:
+                System.exit(0);
+        }
+        sc.close();
+    }
+
+    public static void displayMenu() {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
     }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,12 +3,10 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void greeting() {
-        Scanner sc = new Scanner(System.in);
+    public static void greeting(Scanner sc) {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         var name = sc.next();
         System.out.println("Hello, " + name + "!");
-        sc.close();
     }
 }
