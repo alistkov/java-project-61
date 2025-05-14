7440aebf58c20e512bbde11932b718f5c2cd85e9
alistkov <antonlistkov@gmail.com>
2025-05-14 23:54:56 +0200

    update App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -18,9 +18,16 @@ public class App {
     private static final int PRIME_GAME = 6;
 
     public static void main(String[] args) {
-        displayMenu();
-
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
 
         var playerChoice = sc.nextInt();
 
@@ -50,17 +57,5 @@ public class App {
                 throw new IllegalStateException("Unexpected value: " + playerChoice);
         }
         sc.close();
-    }
-
-    public static void displayMenu() {
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
-        System.out.print("Your choice: ");
     }
 }
