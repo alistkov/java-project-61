a1f363bc0a51b5093c99667b4b537c4ce4e7f63d
alistkov <alistkov.dev@gmail.com>
2026-08-28 23:01:14 +0200

    update gradle version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.github.ben-manes.versions") version "0.61.0"
 }
 
 group = "hexlet.code"
@@ -9,12 +10,4 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
+dependencies {}
