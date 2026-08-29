14ceae41bb342463887ba9db6656fc085fdc09fb
alistkov <alistkov.dev@gmail.com>
2026-08-29 18:35:01 +0200

    get scanner instance from app

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,12 +3,10 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void run() {
+    public static void run(Scanner sc) {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name?: ");
-        var sc = new Scanner(System.in);
         var name = sc.next();
         System.out.println("Hello, " + name + "!");
-        sc.close();
     }
 }
