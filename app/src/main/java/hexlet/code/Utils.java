c135a99f7920c19272b8a4e236564735e63721eb
alistkov <alistkov.dev@gmail.com>
2026-08-29 18:34:44 +0200

    add even game

---
+++ app/src/main/java/hexlet/code/Utils.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import java.util.Random;
+
+public class Utils {
+    private static final Random RANDOM = new Random();
+
+    public static int generateRandomNumber(int min, int max) {
+        return RANDOM.nextInt(min, max);
+    }
+}
