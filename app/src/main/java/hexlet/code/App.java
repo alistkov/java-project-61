a84c96d9c6035ef079b060ef509410c9b3e0cc55
alistkov <alistkov.dev@gmail.com>
2026-08-29 15:57:39 +0200

    setup game menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.run();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n0 - Exit");
+        System.out.print("Your choice: ");
+
+        var sc = new Scanner(System.in);
+        var game = sc.next();
+
+        switch (game) {
+            case "0" -> System.out.println("See you next time!");
+            case "1" -> Cli.run();
+            default -> System.out.println("You have entered an incorrect value.");
+        }
+        sc.close();
     }
 }
