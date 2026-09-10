09e71aed70cdc85bbd814129c178ea9a5231aaaf
alistkov <alistkov.dev@gmail.com>
2026-09-10 14:03:37 +0200

    add calc game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
 import java.util.Scanner;
 
@@ -7,10 +8,17 @@ public class App {
     private static final String EXIT_OPTION = "0";
     private static final String GREETING_OPTION = "1";
     private static final String EVEN_GAME = "2";
+    private static final String CALC_GAME = "3";
 
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("0 - Exit\n1 - Greet\n2 - Even");
+        System.out.println(
+                """
+                    0 - Exit
+                    1 - Greet
+                    2 - Even
+                    3 - Calc
+                """);
         System.out.print("Your choice: ");
 
         var sc = new Scanner(System.in);
@@ -20,6 +28,7 @@ public class App {
             case EXIT_OPTION -> System.out.println("See you next time!");
             case GREETING_OPTION -> Cli.run(sc);
             case EVEN_GAME -> Even.run();
+            case CALC_GAME -> Calc.run();
             default -> System.out.println("Unexpected selection.");
         }
         sc.close();
