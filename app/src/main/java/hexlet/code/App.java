735d1a6ea4b111b897f66835666e848b67ed6298
alistkov <alistkov.dev@gmail.com>
2026-09-18 00:54:29 +0200

    fix games menu output

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -16,13 +16,13 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println(
                 """
-                    1 - Greet
-                    2 - Even
-                    3 - Calc
-                    4 - GCD
-                    5 - Progression
-                    6 - Prime
-                    0 - Exit
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit
                 """);
         System.out.print("Your choice: ");
 
